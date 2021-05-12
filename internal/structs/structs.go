c3a8f42d157499952bc19a58dda974a5d99b0735
sunlazor <a.matveychuk@ilp-agency.ru>
2021-05-12 18:28:28 +0300

    Savepoint

--- internal/structs/structs.go
+++ internal/structs/structs.go
@@ -19,3 +19,17 @@ type Constellation struct {
 	NameID          uint32    `yaml:"nameID"`
 	Radius          float64   `yaml:"radius"`
 }
+
+type SolarSystem struct {
+	Border        bool      `yaml:"border"`
+	Center        []float32 `yaml:"center"`
+	Corridor      bool      `yaml:"corridor"`
+	Fringe        bool      `yaml:"fringe"`
+	Hub           bool      `yaml:"hub"`
+	International bool      `yaml:"international"`
+	Luminosity    float64   `yaml:"luminosity"`
+	Max           []float32 `yaml:"max"`
+	Min           []float32 `yaml:"min"`
+	NameID        uint32    `yaml:"nameID"`
+	Radius        float64   `yaml:"radius"`
+}
--- main.go
+++ main.go
@@ -54,5 +54,4 @@ func archive() {
 			}
 		}
 	}
-
 }
