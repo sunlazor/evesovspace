448d61c2d9c0c03210e00b51129b63ac4c757003
sunlazor <a.matveychuk@ilp-agency.ru>
2021-05-13 18:55:13 +0300

    savepoint

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"archive/zip"
 	"evesovspace/internal/structs"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"io/fs"
+	"log"
 	"os"
 	"strings"
 
@@ -12,7 +16,43 @@ import (
 )
 
 func main() {
-	archive()
+	const width, height = 1000, 1000
+
+	// Create a colored image of the given width and height.
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			img.Set(x, y, color.NRGBA{R: 0, G: 0, B: 0, A: 255})
+		}
+	}
+
+	// for y := 0; y < height; y++ {
+	// 	for x := 0; x < width; x++ {
+	// 		img.Set(x, y, color.NRGBA{
+	// 			R: uint8((x + y) & 255),
+	// 			G: uint8((x + y) << 1 & 255),
+	// 			B: uint8((x + y) << 2 & 255),
+	// 			A: 255,
+	// 		})
+	// 	}
+	// }
+
+	f, err := os.Create("image.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := png.Encode(f, img); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	// archive()
 }
 
 func archive() {
